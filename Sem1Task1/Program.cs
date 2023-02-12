e178024ae89db994caaf75cf99c89d12fdc4b996
AlekseyAndreev <Alex3080@mail.ru>
2023-02-12 11:06:37 +0300

    Sem1 Task 1

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1
+// Напишите программу, которая на вход приимает два числа и проверяет является ли
+// первое число, квадратом второго.
+
+// ввод числа 1
+Console.WriteLine("Введите число 1: ");
+string? numLine1 = Console.ReadLine();
+//ввод числа 2
+Console.WriteLine("Введите число 2: ");
+string? numLine2 = Console.ReadLine();
+if (numLine1 != null && numLine2 != null)
+{
+// Пасрсим
+    int num1 = int.Parse(numLine1);
+    int num2 = int.Parse(numLine2);
+    if (num2 * num2 == num1)
+    {
+        Console.WriteLine("Да, является! ");
+    }    
+    else
+    {
+        Console.WriteLine("Нет, не является! ");
+    }    
+}
