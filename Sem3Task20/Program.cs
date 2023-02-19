568427ffa2c328d1e333ed16b344ffc17af03bf5
AlekseyAndreev <Alex3080@mail.ru>
2023-02-19 13:27:48 +0300

    Sem3 Task 20

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача № 20
+// Напишите программу которая принимает на вход координаты двух
+// точек и находит растояние между этими двумя точками. В 2D пространстве.
+
+// Метод считывает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// Метод выводит данные пользователя
+void PrintData(string msg, double val)
+{
+    Console.WriteLine(msg+val);
+}
+// Метод находит растояние между точками на плоскости
+double CalcLen2D(int x1, int x2, int y1, int y2)
+{
+    return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
+int x1 = ReadData("Введите координату х точки А: ");
+int y1 = ReadData("Введите координату y точки А: ");
+int x2 = ReadData("Введите координату х точки B: ");
+int y2 = ReadData("Введите координату y точки B: ");
+
+double res = CalcLen2D(x1,x2,y1,y2);
+PrintData("Расстояние между точками А и В: ", res);
