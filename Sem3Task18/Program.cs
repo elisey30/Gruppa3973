6e9f3049d62ed7653815d5600abac89b5fe13737
AlekseyAndreev <Alex3080@mail.ru>
2023-02-19 10:24:03 +0300

    Sem3 Task 18

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача № 18
+// Напишите программу которая по заданному номеру четвертти показывает
+// диапозон возможных координат точки этой четверти. 
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// Метод определяет координаты точки по номеру четверти
+void PrintAnsor(int num)
+{
+    if (num > 0 && num< 5)
+    {
+    if (num == 1) Console.WriteLine("x > 0, y > 0");
+    if (num == 2) Console.WriteLine("x > 0, y < 0");
+    if (num == 3) Console.WriteLine("x < 0, y < 0");
+    if (num == 4) Console.WriteLine("x < 0, y > 0");
+    }
+    else
+    {
+        Console.WriteLine("Неверная четверть: ");
+    }
+}
+
+int quarter = ReadData("Введите номер четверти:");
+PrintAnsor (quarter);
