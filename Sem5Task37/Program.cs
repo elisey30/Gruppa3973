50be16eebb6bed16ee4993e6190a1b7964d27b20
AlekseyAndreev <Alex3080@mail.ru>
2023-02-28 17:36:30 +0300

    Sem5 Task 37

---
+++ Sem5Task37/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача №37
+// Найдите произведение пар чисел в одномерном массиве,
+// парой считаются первый и последний элемент, второй и предпоследний и т.д.
+// результат запишите в новый массив
+
+
+//Метод генерации рандомный чисел с корректировкой входных данных
+int[] Gen1DArr(int len, int min, int max)
+{
+    if (min > max)
+    {
+        int buf = min;
+        min = max;
+        max = buf;
+    }
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+//Метод вывода обрамления
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    Console.Write($"{arr[i]}, ");
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+} 
+//Метод который умножает первую и последнюю, второй и предпоследний и т д пару чисел в масиве
+int[] ConvertArr(int[] arr)
+{
+    int[] butArr = new int[(arr.Length/2)+1];
+    for(int i = 0; i <((arr.Length/2)+1);i++)
+    {
+        butArr[i] = arr[i]*arr[arr.Length -1 - i];
+    }
+    return butArr;
+}
+
+int[] msg = Gen1DArr(6,1,9);
+Print1DArr(msg);
+int [] newArr = ConvertArr(msg);
+Print1DArr(newArr);
