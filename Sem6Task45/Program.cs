41d83abd5f31855c917be01e029b6907fedd7c6d
AlekseyAndreev <Alex3080@mail.ru>
2023-03-02 18:56:24 +0300

    Sem6 Task 45

---
+++ Sem6Task45/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача No45 
+// Напишите программу, которая будет создавать копию заданного одномерного 
+// массива с помощью поэлементного копирования. 
+ 
+// Метод генерации массива 
+int[] Gen1DArr(int len, int minValue, int maxValue) 
+{ 
+    // Блок корректировкивходных данных 
+    if (minValue > maxValue) 
+    { 
+        int buf = minValue; 
+        minValue = maxValue; 
+        maxValue = buf; 
+    } 
+ 
+    Random rnd = new Random(); 
+    int[] arr = new int[len]; 
+ 
+    for (int i = 0; i < arr.Length; i++) 
+    { 
+        arr[i] = rnd.Next(minValue, maxValue); 
+    } 
+    return arr; 
+} 
+// Метод печати масива 
+void Print1DArr(int[] arr) 
+{ 
+    Console.Write("["); 
+ 
+    for (int i = 0; i < arr.Length - 1; i++) 
+    { 
+        Console.Write(arr[i] + ", "); 
+    } 
+    Console.WriteLine(arr[arr.Length - 1] + "]"); 
+} 
+//Метод копирования массива 
+int[] CopyArrayStandartTools(int[] inputArr) 
+{ 
+    int[] outArr = new int[inputArr.Length]; 
+    inputArr.CopyTo(outArr,0); 
+    return outArr; 
+} 
+ 
+//Метод копирования массива универсальный 
+// object[] CopyArrayStandartTools(params object[] inputArr) 
+// { 
+//     object[] outArr = new object[inputArr.Length]; 
+//     inputArr.CopyTo(outArr,0); 
+//     return outArr; 
+// } 
+int[] testArr =Gen1DArr(40,1,50); 
+Print1DArr(testArr); 
+int[] testArrnew = CopyArrayStandartTools(testArr); 
+Print1DArr(testArrnew);
