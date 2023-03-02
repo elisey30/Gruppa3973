3f1e9dbfa185464cd2cb7ab79f0b81cab69bfa86
AlekseyAndreev <Alex3080@mail.ru>
2023-03-02 18:38:34 +0300

    Sem6 Task 42

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача № 42
+// Напишите программу которая будет преобразовывать десятичное 
+// число в двоичное
+
+
+// Метод считываем данные с пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ??"0");
+}
+//Метод вывода результата
+void PrintData(string msg, string num)
+{
+    Console.WriteLine(msg+num);
+} 
+// Метод который преобразовывает десятичное число в двоичное
+string DecToBin(int num)
+{
+    string res = string.Empty;
+    while(num>0)
+    {
+        res = num % 2 + res;
+        num = num/2; 
+    }
+    return res;
+}
+
+int mess = ReadData("Введите число; ");
+PrintData("Число в двоичной системе будет равно: ", DecToBin(mess));
+
