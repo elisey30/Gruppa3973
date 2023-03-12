8bee16e654cb5fc8e34fccb2167eec49fc824e0d
AlekseyAndreev <Alex3080@mail.ru>
2023-03-12 11:02:06 +0300

    Sem9 Task 69

---
+++ Sem9Task69/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача № 69
+// Напишите программу, которая будет принимать два числа а,в
+// и она возведет а в степень в с помощью рекурсии.
+
+// Метод ввода данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+// Печать результата 
+void PrintData(long msg)
+{
+    Console.WriteLine(msg);
+}
+// Метод который возводит А в степень В с помощью рекурсии
+long RecPow(int a, int b)
+{
+    if( b <= 1) return a;
+    return a * RecPow(a,b-1);
+}
+
+int numberA = ReadData("Введите число A: ");
+int numberB = ReadData("Введите число B: ");
+long res = RecPow(numberA,numberB);
+PrintData(res);
