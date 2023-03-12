7d56e50b542b60bfc86bb4c3bcebcb736f5fc813
AlekseyAndreev <Alex3080@mail.ru>
2023-03-12 10:29:31 +0300

    Sem9 Task 67

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача № 67
+// Напишите программу кторорая принимает на вход число,
+// и возвращает сумму этого числа.
+
+// Метод ввода данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+// Печать результата 
+void PrintData(int msg)
+{
+    Console.WriteLine(msg);
+}
+// Метод который возвращает сумму этих чисел
+int RecSumDig(int num)
+{
+    if(num == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num % 10 + RecSumDig(num/10);
+    }
+}
+
+int test = ReadData("Введите число:  ");
+int res = RecSumDig(test);
+PrintData(res);
